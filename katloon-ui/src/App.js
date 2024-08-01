bac54d56a97226290965af4bfd27780bc5621710
Waris Walia <wariswalia2002@gmail.com>
2024-07-31 23:37:29 -0400

    Setup UI

---
+++ katloon-ui/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Category from './Category';
+
+function App() {
+  return (
+    <div className="App">
+      <Category/>
+    </div>
+  );
+}
+
+export default App;
